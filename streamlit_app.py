0efc76e6c1e417e30dd395af6be4f8e0983a06cd
rakeshtds <rakeshtdsharma@yahoo.com>
2024-05-20 10:04:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ except ImportError:
 
 st.set_page_config(page_title="Chat easily with Lenders Data, powered by Sylphia Consulting(SCi)", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
-st.title("Chat with the AssetDirect docs, powered by Sylphia Consulting ")
+st.title("Chat with the AssetDirect docs, powered by Sylphia Consulting(SCi) ")
     
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
