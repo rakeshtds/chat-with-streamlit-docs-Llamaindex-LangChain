46f8359d811ce1de85f5bf3b929477cccca9c50e
rakeshtds <rakeshtdsharma@yahoo.com>
2024-05-20 12:25:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ def load_data():
         docs = reader.load_data()
         # llm = OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert o$
         # index = VectorStoreIndex.from_documents(docs)
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-4-turbo", temperature=0, system_prompt="You are an data analyst working in Asset Direct of Canada Inc, a SAAS provider, was founded in 2015 in London, Ontario. Asset Direct mission is to bring accessibility and flexibility to the world of personal finance. Allowing those who are credit invisible to establish a proper credit history, and providing choice to those currently restricted to one institution for all of their financial product and insurance needs. Your job is to answer questions based on the Lenders and their data. Assume that all questions are related to the Asset Direct Lenders. Keep your answers based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
